9eed36102b004035369820bdb8356991df7525c7
BakyaNallamuthu <bakyanallamuthu@gmail.com>
2025-05-26 01:04:11 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiple_Interface
+{
+  
+
+interface IFirstInterface
+    {
+        void Display();
+    }
+
+    interface ISecondInterface
+    {
+        void Display();
+    }
+
+    class MyClass : IFirstInterface, ISecondInterface
+    {
+        void IFirstInterface.Display()
+        {
+            Console.WriteLine("First Interface:IFirstInterface");
+        }
+
+        void ISecondInterface.Display()
+        {
+            Console.WriteLine("Second Interface:Display from ISecondInterface");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            MyClass obj = new MyClass();
+            ((IFirstInterface)obj).Display();
+            ((ISecondInterface)obj).Display();
+        }
+    }
+
+}
+
